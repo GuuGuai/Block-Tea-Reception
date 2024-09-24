c3f9fd1745822bcc947282fd9fa0dc9a18c497ec
L33Z22L11 <L33Z22L11@gmail.com>
2024-09-24 11:26:45 +0800

    feat: 提升项目规范程度

---
+++ docs/.vitepress/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <span />
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    padding-top: 2rem;
+    font-size: 0.8rem;
+    text-align: center;
+}
+
+a {
+    color: var(--vp-c-brand);
+}
+</style>
